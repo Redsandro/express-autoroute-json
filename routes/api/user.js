0fb44a3dbd67ce2687eb462cfcf4321088d1d7ce
Sander Steenhuis <redsandro@gmail.com>
2018-12-21 19:10:43 +0100

    Deprecate custom serializer

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,16 @@
 module.exports = ({mongoose}) => ({
 	schema	: new mongoose.Schema({
 		name	: String,
-		email	: String
+		email	: String,
+
+		myTests	: [{
+			type	: mongoose.Schema.Types.ObjectId,
+			ref		: 'Test'
+		}],
+		lastUsed	: {
+			type	: mongoose.Schema.Types.ObjectId,
+			ref		: 'Test'
+		}
 	}),
 
 	create	: {},
